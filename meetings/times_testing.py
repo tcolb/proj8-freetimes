565528bdf4ec96274bb5efb9cb0b444f020e8a5f
Tristan Colby <tristan.colb@gmail.com>
2017-11-20 18:57:14 -0800

    weird inbetween bug, two events edge to edge

--- meetings/times_testing.py
+++ meetings/times_testing.py
@@ -27,49 +27,10 @@ def test_chunk_initialization():
     chunk = Chunk(time_1, time_2)
 
     # Testing start and end times
-    assert chunk.start() == time_1
-    assert chunk.end() == time_2
+    assert chunk._begin == time_1
+    assert chunk._end == time_2
 
     print("Chunk initialization working!")
-
-
-def test_chunk_subtraction():
-
-    # Test __sub__ case 1
-    assert (chunk_2 - chunk_1)[0] == Chunk(arrow.get("2017-11-16T17:00:00-08:00"),
-                                           arrow.get("2017-11-16T20:00:00-08:00"))
-
-    # Test __sub__ case 2
-    assert (chunk_1 - chunk_2)[0] == Chunk(arrow.get("2017-11-16T09:00:00-08:00"),
-                                           arrow.get("2017-11-16T12:00:00-08:00"))
-
-    # Test __sub__ case 3
-    assert (chunk_1 - chunk_3)[0] == None
-
-    # Test __sub__ case
-    assert (chunk_3 - chunk_1) == [ Chunk(arrow.get("2017-11-16T02:00:00-08:00"), arrow.get("2017-11-16T09:00:00-08:00")),
-                                    Chunk(arrow.get("2017-11-16T17:00:00-08:00"), arrow.get("2017-11-16T20:00:00-08:00")) ]
-
-    print("Chunk subtraction working!")
-
-
-def test_chunk_addition():
-
-    # Test __add__ case 1
-    assert (chunk_2 + chunk_1) == Chunk(arrow.get("2017-11-16T09:00:00-08:00"),
-                                        arrow.get("2017-11-16T20:00:00-08:00"))
-
-    # Test __add__ case 2
-    assert (chunk_1 + chunk_2) == Chunk(arrow.get("2017-11-16T09:00:00-08:00"),
-                                        arrow.get("2017-11-16T20:00:00-08:00"))
-
-    # Test __add__ case 3
-    assert (chunk_3 + chunk_1) == chunk_3
-
-    # Test __add__ case 4
-    assert (chunk_1 + chunk_3) == chunk_3
-
-    print("Chunk addition working!")
 
 
 #
@@ -79,32 +40,20 @@ def test_chunk_addition():
 
 def test_block_initialization():
 
-    chunk_4 = Chunk(arrow.get("2017-11-16T02:00:00-08:00"),
-                    arrow.get("2017-11-16T06:00:00-08:00"))
-    chunk_5 = Chunk(arrow.get("2017-11-16T04:00:00-08:00"),
-                    arrow.get("2017-11-16T08:00:00-08:00"))
-    chunk_6 = Chunk(arrow.get("2017-11-16T10:00:00-08:00"),
-                    arrow.get("2017-11-16T18:00:00-08:00"))
-    chunk_7 = Chunk(arrow.get("2017-11-16T12:00:00-08:00"),
-                    arrow.get("2017-11-16T20:00:00-08:00"))
+    cks = [ chunk_4 = Chunk(arrow.get("2017-11-16T02:00:00-08:00"),
+                            arrow.get("2017-11-16T06:00:00-08:00"))
+                      Chunk(arrow.get("2017-11-16T04:00:00-08:00"),
+                            arrow.get("2017-11-16T08:00:00-08:00"))
+                      Chunk(arrow.get("2017-11-16T10:00:00-08:00"),
+                            arrow.get("2017-11-16T18:00:00-08:00"))
+                      Chunk(arrow.get("2017-11-16T12:00:00-08:00"),
+                            arrow.get("2017-11-16T20:00:00-08:00")) ]
+
 
     # Test to see if blocks combine chunks at init
-    block = Block([chunk_4, chunk_5, chunk_6, chunk_7])
-    #assert  == [ Chunk(arrow.get("2017-11-16T02:00:00-08:00"),
-    #                                                              arrow.get("2017-11-16T20:00:00-08:00")) ]
-    print(block.chunks())
+    block = Block()
+    [ block.append(chunk) for chunk in cks ]
+    assert block.chunks() == [ Chunk(arrow.get("2017-11-16T02:00:00-08:00"),
+                                     arrow.get("2017-11-16T20:00:00-08:00")) ]
+
     print("Block initialiation working!")
-
-
-def test_block_subtraction():
-
-    # Test to see if block subtraction works
-    return None
-
-
-if __name__ == "__main__":
-    test_chunk_initialization()
-    test_chunk_subtraction()
-    test_chunk_addition()
-
-    test_block_initialization()
